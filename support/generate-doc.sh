19ef0ac33ab18ba9a895a178eb84279394dce2e5
Julien ÉLIE <Julien-Elie@users.noreply.github.com>
2023-09-30 15:21:45 +0200

    Use a README file in Markdown, for best display

--- support/generate-doc.sh
+++ support/generate-doc.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 #
-# Support script to generate documentation for signcontrol.py, suitable
-# to be read as text.
+# Generate a README.md file in a Markdown syntax suitable to be displayed
+# in the Github web interface.  It extracts the POD documentation from
+# signcontrol.py, and performs a few style changes.
 
-pod2text --sentence --loose ../signcontrol.py > ../README
+(
+    pod2markdown ../signcontrol.py \
+        | sed -e 's/# NAME/# usenet-signcontrol/' \
+            -e '2 i \
+\
+> Copyright (c) 2007-2009, 2011, 2016, 2019-2020, 2023 Julien ÉLIE \
+\
+This software is distributed under the MIT license.  Please see the\
+[License](#license) section below for more information.
+' \
+            -e '3,8d' \
+            -e 's/# DESCRIPTION/## Description/' \
+            -e 's/# SUPPORT/## Support/' \
+            -e 's/# SOURCE REPOSITORY/## Source Repository/' \
+            -e 's/# CONFIGURATION FILE/## Configuration File/' \
+            -e 's/# USEFUL RESOURCES/## Useful Resources/' \
+            -e 's/# LICENSE/## License/' \
+            -e 's/    //' \
+            -e 's/^Copyright (c)/> Copyright (c)/' \
+            -e 's/^Permission is/> Permission is/' \
+            -e 's/^The above copyright/> The above copyright/' \
+            -e 's/^THE SOFTWARE/> THE SOFTWARE/' \
+            -e '/# HISTORY/,+7d' \
+        | awk '/> (Permission|The|THE)/ { prev = ">" } \
+            { if (NR > 1) print prev } { prev = $0 }'
+) >../README.md
